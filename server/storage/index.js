ca0466cb9c95042a6767beab546020e875eea034
DiegoSuquitana <35553620+DiegoSuquitana@users.noreply.github.com>
2020-07-19 22:57:04 -0500

    Update index.js
    Cambio para la conexion data.json

--- server/storage/index.js
+++ server/storage/index.js
@@ -1,14 +1,15 @@
-var fs = require('fs'),
-    path = require('path')
-const { resolve } = require('path')
-const { rejects } = require('assert')
+const fsd = require('fs'),
+//        $ = require('jquery'),
+    pathd = require('path');
 
 //OBJETO DE RETORNA LA DATA OBTENIDA DEL ARCHIVO data.json
 module.exports = {
-    getData: function(dataType){
-        let dataPath = __dirname + path.join('/data/data.json')
+    getDataAll: (dataType) => {
+        var self = this;
+        //let dataPath = __dirname + pathd.join('/data/data.json');
+        let dataPath = pathd.join(__dirname, '/data/data.json');
         return new Promise((resolve, rejects) => {
-            fs.readFile(dataPath, 'utf8', (err, readData) =>{
+            fsd.readFile(dataPath, 'utf8', (err, readData) =>{
                 if(err) rejects(err)
                 resolve(JSON.parse(readData))
             })
