8416725223535cac3900b3326a77c36ca9689e6f
DiegoSuquitana <35553620+DiegoSuquitana@users.noreply.github.com>
2020-07-19 23:00:08 -0500

    Update requestRouting.js

---
+++ server/requestRouting.js
@@ -0,0 +1,13 @@
+/*var express = require('express')
+var path = require('path')
+var Router = express.Router()
+
+var viewsPath = path.join(_dirname, '../')+'public/'
+
+Router.get('/index', function(req, res){
+    res.sendFile(viewsPath + 'index.html')
+    res.end
+})
+
+module.exports = Router
+*/
