c939e9b8f7b1912e6b425b3d4b81074d2d0f15a7
DiegoSuquitana <35553620+DiegoSuquitana@users.noreply.github.com>
2020-07-24 23:31:08 -0500

    Update index.js

--- server/router/index.js
+++ server/router/index.js
@@ -1,6 +1,16 @@
-var express = require('express')
+/*var express = require('express')
 const Storage = require('../storage')
 var Router = express.Router()
+,io = require('socket.io')
+*/
+var  bodyParser = require('body-parser')
+    ,http = require('http')
+    ,express = require('express')
+    ,Router = express.Router()
+    ,jQuery = require('jquery')
+    ,io = require('socket.io')
+    ,Storage = require('../storage');
+
 
 Router.get('/search', (req,res)=>{
     Storage.getDataAll('data')
@@ -11,5 +21,37 @@ Router.get('/search', (req,res)=>{
             })
 
 })
+
+Router.get("/ciudades", (req, res)=>{
+    Storage.getData('ciudades')
+            .then(ciudades=>{
+              var ciudadesUnique = [];
+              ciudades.forEach(function(o) {
+                if(ciudadesUnique.indexOf(o.Ciudad) < 0){
+                   ciudadesUnique.push(o.Ciudad);
+                }
+              })
+              res.json(ciudadesUnique)
+            })
+            .catch(error=>{
+              res.sendStatus(500).json(error);
+            })
+  })
+  
+  Router.get("/tipos", (req, res)=>{
+    Storage.getData('tipos')
+            .then(tipos=>{
+              var tiposUnique = [];
+              tipos.forEach(function(o) {
+                if(tiposUnique.indexOf(o.Tipo) < 0){
+                   tiposUnique.push(o.Tipo);
+                }
+              })
+              res.json(tiposUnique)
+            })
+            .catch(error=>{
+              res.sendStatus(500).json(error);
+            })
+  })
 
 module.exports = Router;
