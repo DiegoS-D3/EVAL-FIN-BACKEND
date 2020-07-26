d39ff719728993c2ba1d7e87227f8be4757eb690
DiegoSuquitana <35553620+DiegoSuquitana@users.noreply.github.com>
2020-07-25 23:37:30 -0500

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -9,18 +9,18 @@ $("#rangoPrecio").ionRangeSlider({
   prefix: "$"
 })
 
-function visualizar(){
-  let panel = $(".clasificado")
-  panel.show()
-  panel.style.display("block")
-}
+var ban = 0;
 
 function setSearch() {
   let busqueda = $('#checkPersonalizada')
   busqueda.on('change', (e) => {
     if (this.customSearch == false) {
       this.customSearch = true
+      alert("FILTRO DESACTIVADO")
+      ban=0;
     } else {
+      alert("FILTRO ACTIVADO")
+      ban=1;
       this.customSearch = false
     }
     $('#personalizada').toggleClass('invisible')
@@ -38,17 +38,25 @@ setSearch();
 
       ,Init: function(){
         var self = this;
+        $('.clasificado').hide()
         self.loadCiudades()
         self.loadTipos()
-        self.loadInmuebles()
         $('.clasificado').empty("");
         $("#buscar").click(()=>{
-          visualizar();
+          //self.loadCiudades()
+          //self.loadTipos()
+          if(ban == 1){
+            self.loadInmuebles()
+          }else{
+          self.loadInmuebles()
+          alert(ban);
+          $('.clasificado').show(); 
           var ciudad = $("#ciudad").val();
           var tipo = $("#tipo").val();
           var precio = $("#rangoPrecio").val();
           var filter = {Ciudad: ciudad, Tipo: tipo, Precio: precio}
           self.customSearch(filter);
+        }
         })
         
         $("#tipo, #ciudad, #rangoPrecio").change(()=>{
@@ -157,89 +165,3 @@ setSearch();
   },1000);
 })(document, window, document, jQuery);
 
-
-/*
-function ajaxRequest(url, type, data){
-  return $.ajax({
-                url: url
-                ,type: type
-                ,data: data
-              })
-}
-
-function verTodos() {
-  let busqueda = $('#buscar')
-  busqueda.on('click',(e)=>{
-    var self = this;
-    alert('click')
-    self.ajaxRequest('http://localhost:3000/router/search','get','json')
-    /*$.ajax({
-      url:'http://localhost:3000/router/search',
-      type: 'get',
-      dataType: 'json'
-    })*/
-    /*
-    .done((data)=>{
-      alert('done');
-      if(!data.error){
-        console.log(data);
-        alert('data');
-        var $inmuebles = $('.lista');
-        $inmuebles.html("");
-        var newHtml = ''
-        $.each(data, (i,inmueble)=>{
-          alert('inmuebles '+ inmueble.Direccion)
-          newHtml = `<div class="card horizontal">
-          <div class="card-image">
-              <img src="http://localhost:3000/img/home.jpg">
-          </div>
-          <div class="card-stacked">
-              <div class="card-content">
-                  <div> <p><strong>Direccion: </strong>${ inmueble.Direccion }</p> </div>
-                  <div> <p><strong>Ciudad: </strong>${ inmueble.Ciudad }</p> </div>
-                  <div> <p><strong>Telefono: </strong>${ inmueble.Telefono }</p> </div>
-                  <div> <p><strong>Código postal: </strong>${ inmueble.Codigo_Postal }</p> </div>
-                  <div> <p><strong>Precio: </strong>${ inmueble.Precio }</p> </div>
-                  <div> <p><strong>Tipo: </strong>${ inmueble.Tipo }</p> </div>
-              </div>
-          </div>
-      </div>`
-      $('.lista').html((data.datos));
-      /*
-           newHtml = self.$clasificadoTemp.html().replace(":Direccion:",inmueble.Direccion)
-                                                    .replace(":Ciudad:",inmueble.Ciudad)
-                                                    .replace(":Telefono:",inmueble.Telefono)
-                                                    .replace(":Codigo_Postal:",inmueble.Codigo_Postal)
-                                                    .replace(":Precio:",inmueble.Precio)
-                                                    .replace(":Tipo:",inmueble.Tipo);
-                                                    alert('html');
-          var $control = self.$clasificadoTemp.clone().html(newHtml);
-          */
-         /*
-          if(filter===undefined){
-            alert('if');
-              $inmuebles.append( $control );
-          }
-          else{
-            alert('else');
-            var show = (filter.Ciudad ===undefined || filter.Ciudad =="" || filter.Ciudad == inmueble.Ciudad);
-            var show = show && (filter.Tipo ===undefined || filter.Tipo =="" || filter.Tipo == inmueble.Tipo);
-            var precio = filter.Precio.split(";");
-            var precioInmueble = inmueble.Precio.replace("$","").replace(",","");
-            var show = show && ( precioInmueble >= precio[0] && precioInmueble <= precio[1]);
-            console.log(`Ciudad:${filter.Ciudad}, Tipo: ${filter.Tipo}, Precio: ${precio}, precioInmueble: ${precioInmueble}, Show: ${show}`);
-            if(show){
-              alert('if2');
-              $inmuebles.append( $control );
-            }
-          }
-        })
-        
-      }
-    })
-  })
-}
-
-setSearch()
-verTodos()
-*/
